9ee14ec55d4d3922b54a5347bd2bb1ee231c4bb6
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2023-09-13 11:33:04 +0530

    #3

--- SIH_PROJECT/src/Components/login.tsx
+++ SIH_PROJECT/src/Components/login.tsx
@@ -1,50 +1,108 @@
-import "../Components/style/loginstyle.css";
-import img2 from "../assets/one.svg"
-import { Link } from 'react-router-dom';
+import { useState, ChangeEvent, FormEvent } from "react";
+import { Link } from "react-router-dom";
+import img2 from "../assets/two.svg";
+import "../Components/style/loginstyle.css"
 
-function Login(){
-    return(
-        <>
-            <div className="login-container">
-                <div className="login-left-container">
-                    <h1>Welcome back !</h1>
-                    <p className="login-top-title">Legistrive: Bridging the Gap Between Legal Talent and Opportunities, One Connection at a Time.</p>
-                    
-                    <div className="login-left-container-sub">
-                    <input type="text" placeholder="Username"/>
-                    <input type="password" placeholder="Password"/>
+interface FormData {
+  username: string;
+  password: string;
+}
 
-                    <button className="login-btn-login">
-                        Login
-                    </button>
-                    </div>
-                   
-                    
-                    <span className="login-text">Or Continue With</span>
-                    <div className="login-icons">
-                    <div className="icon"><i className="fa-brands fa-google"></i></div>
-                    <div className="icon"><i className="fa-brands fa-linkedin"></i></div>
-                    <div className="icon"><i className="fa-brands fa-facebook"></i></div>
-                    </div>
-                    
+function Login() {
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+    password: "",
+  });
 
-                    <p className="login-sub-title-login">Not A Member Register here!</p>
-                    <Link  className="login-register-btn"to="/">Register here!</Link>
-                    
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-                </div>
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
 
-                <div className="login-container-right">
-                    
-                </div>
+    //API LINK 
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-                <div className="login-right-container">
-                    <img src={img2} alt="img" />
-                    <p>Unlocking Legal Opportunities and Talent with Legistrive.</p>
-                </div>
+      if (response.ok) {
+        console.log("Data stored successfully!");
+      } else {
+        console.error("Failed to store data");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="login-container">
+        <div className="login-left-container">
+          <h1>Welcome back!</h1>
+          <p className="login-top-title">
+            Legistrive: Bridging the Gap Between Legal Talent and Opportunities, One Connection at a Time.
+          </p>
+
+          <div className="login-left-container-sub">
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+
+            <button className="login-btn-login" onClick={handleSubmit}>
+              Login
+            </button>
+          </div>
+
+          <span className="login-text">Or Continue With</span>
+          <div className="login-icons">
+            <div className="icon">
+              <i className="fa-brands fa-google"></i>
             </div>
-        </>
-    )
+            <div className="icon">
+              <i className="fa-brands fa-linkedin"></i>
+            </div>
+            <div className="icon">
+              <i className="fa-brands fa-facebook"></i>
+            </div>
+          </div>
+
+          <p className="login-sub-title-login">Not A Member? Register here!</p>
+          <Link className="login-register-btn" to="/">
+            Register here!
+          </Link>
+        </div>
+
+        <div className="login-container-right"></div>
+
+        <div className="login-right-container">
+          <img src={img2} alt="img" />
+          <p>Unlocking Legal Opportunities and Talent with Legistrive.</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Login;
--- SIH_PROJECT/src/Components/register.tsx
+++ SIH_PROJECT/src/Components/register.tsx
@@ -1,36 +1,106 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+import { Link } from "react-router-dom";
+import img1 from "../assets/two.svg";
 import "../Components/style/registerstyle.css"
-import img1 from "../assets/two.svg"
-import { Link } from 'react-router-dom';
 
-function Register (){
-    return(
-        <>  
+interface FormData {
+  username: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+}
 
-            <div className="register-container">
-                <div className="register-left-container">
-                    <h1>Register Now</h1>
-                    <p className="register-top-title">Legistrive: Bridging the Gap Between Legal Talent and Opportunities, One Connection at a Time.</p>
-                    
-                    <input type="text" placeholder="Username"/>
-                    <input type="email" placeholder="Email"/>
-                    <input type="number" placeholder="Phone Number"/>
-                    <input type="password" placeholder="Password"/>
+function Register() {
+  const [formData, setFormData] = useState<FormData>({
+    username: "",
+    email: "",
+    phoneNumber: "",
+    password: "",
+  });
 
-                    <button className="register-btn-register">
-                        Register
-                    </button>
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-                    <p className="register-sub-title">Already A Member ?</p>
-                    <Link to="/login">Login</Link>
-                </div>
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    // API LINK 
+    try {
+      const response = await fetch("/api/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-                <div className="register-right-container">
-                    <img src={img1} alt="img" />
-                    <p>Unlocking Legal Opportunities and Talent with Legistrive.</p>
-                </div>
-            </div>
-        </>
-    )
+      if (response.ok) {
+        console.log("Registration successful!");
+      } else {
+        console.error("Failed to register");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="register-container">
+        <div className="register-left-container">
+          <h1>Register Now</h1>
+          <p className="register-top-title">
+            Legistrive: Bridging the Gap Between Legal Talent and Opportunities, One Connection at a Time.
+          </p>
+
+          <input
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="phoneNumber"
+            placeholder="Phone Number"
+            value={formData.phoneNumber}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+
+          <button className="register-btn-register" onClick={handleSubmit}>
+            Register
+          </button>
+
+          <p className="register-sub-title">Already A Member?</p>
+          <Link to="/login">Login</Link>
+        </div>
+
+        <div className="register-right-container">
+          <img src={img1} alt="img" />
+          <p>Unlocking Legal Opportunities and Talent with Legistrive.</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Register;
