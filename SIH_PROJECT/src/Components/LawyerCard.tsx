c77003a758384bb25b4578594c1da5a46f8abb6f
rohitchouhan3512 <rohitchouhan3512@gmail.com>
2023-09-19 23:41:46 +0530

    added filter in frontend

--- SIH_PROJECT/src/Components/LawyerCard.tsx
+++ SIH_PROJECT/src/Components/LawyerCard.tsx
@@ -1,50 +1,167 @@
-import React from "react";
-import "../Components/style/LawyearCard.css"; 
+import React, { useState, useEffect } from "react";
+import "../Components/style/LawyearCard.css";
+import Lcard from "./Lcard";
+import axios from "axios";
 
-const LawyerCard = () => {
-  // Dummy lawyer data
-  const lawyerData = {
+interface LawyerData {
+  name: string;
+  address: string;
+  experience: string;
+  skills: string[];
+  rating: number;
+  reviews: number;
+  languages: string[];
+}
+
+const LawyerCard: React.FC = () => {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+  const [location, setLocation] = useState<string[]>([]);
+  const [language, setLanguage] = useState<string[]>([]);
+  const [doFilter, setDoFilter] = useState(false);
+  const [lawyerDataArray, setLawyerDataArray] = useState<LawyerData[]>([]);
+
+  
+  const lawyerData: LawyerData = {
     name: "John Doe",
     address: "123 Main St, City",
     experience: "5 years",
     skills: ["Divorce", "Child Custody", "Mediation"],
     rating: 4.5,
     reviews: 27,
-    languages: ["English", "Spanish"], // Add a languages property
+    languages: ["English", "Spanish"],
   };
 
+  const problemCategory = [
+    "Family Law",
+    "Criminal Defense",
+    "Divorce & Child Custody",
+    "Property & Real Estate",
+    "Employment Issues",
+    "Consumer Protection",
+    "Civil Matters",
+    "Cyber Crime",
+    "Company & Start-Ups",
+    "Other Legal Problem",
+  ];
+
+  const cities = ["New York", "Ahmedabad", "Vadodara", "Vapi"];
+
+  const languages = ["English", "Hindi", "Guj"];
+
+  const handleCategory = (option: string) => {
+    setSelectedOptions((prevSelectedOptions) => {
+      const isOptionSelected = prevSelectedOptions.includes(option);
+      if (isOptionSelected) {
+        return prevSelectedOptions.filter((item) => item !== option);
+      } else {
+        return [...prevSelectedOptions, option];
+      }
+    });
+    setDoFilter(true);
+  };
+
+  const handleCity = (option: string) => {
+    setLocation((prevSelectedCities) => {
+      const isOptionSelected = prevSelectedCities.includes(option);
+      if (isOptionSelected) {
+        return prevSelectedCities.filter((item) => item !== option);
+      } else {
+        return [...prevSelectedCities, option];
+      }
+    });
+    setDoFilter(true);
+  };
+
+  const handleLanguage = (option: string) => {
+    setLanguage((prevSelectedLanguages) => {
+      const isOptionSelected = prevSelectedLanguages.includes(option);
+      if (isOptionSelected) {
+        return prevSelectedLanguages.filter((item) => item !== option);
+      } else {
+        return [...prevSelectedLanguages, option];
+      }
+    });
+    setDoFilter(true);
+  };
+
+  useEffect(() => {
+    // Move the API call logic inside useEffect
+    if (doFilter) {
+      const requestBody = {
+        LOCATION: location,
+        LANGUAGES: language,
+        SPECIALITIES: selectedOptions,
+      };
+      
+      setDoFilter(false);
+
+      const filterAPIUrl = "http://localhost:3000/filterLawyer";
+
+      const fetchData = async () => {
+        try {
+          const response = await axios.post(filterAPIUrl, requestBody);
+          const updatedLawyerDataArray = response.data; // Assuming API response is an array of LawyerData
+          setLawyerDataArray(updatedLawyerDataArray); // Update the array with fetched data
+          console.log("updated Lawyer Data: ", updatedLawyerDataArray);
+        } catch (error) {
+          console.error("Error while fetching data:", error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [doFilter, selectedOptions, location, language]);
+
   return (
-    <div className="lawyer-card">
-      <div className="lawyer-card-header">
-        <div className="lawyer-card-rating">
-          Rating: {lawyerData.rating}
-        </div>
-        <div className="lawyer-card-reviews">
-          Review: {lawyerData.reviews}
-        </div>
+    <div className="lawyer-data">
+      <div className="category-bar">
+        <select
+          value={location}
+          onChange={(e) => handleCity(e.target.value)}
+        >
+          <option value="">Select a city</option>
+          {cities.map((city) => (
+            <option key={city} value={city}>
+              {city}
+            </option>
+          ))}
+        </select>
+
+        <hr className="divider-line" />
+
+        {problemCategory.map((option) => (
+          <label key={option}>
+            <input
+              type="checkbox"
+              value={option}
+              checked={selectedOptions.includes(option)}
+              onChange={() => handleCategory(option)}
+            />
+            {option}
+          </label>
+        ))}
+
+        <hr className="divider-line" />
+
+        {languages.map((option) => (
+          <label key={option}>
+            <input
+              type="checkbox"
+              value={option}
+              checked={language.includes(option)}
+              onChange={() => handleLanguage(option)}
+            />
+            {option}
+          </label>
+        ))}
+
       </div>
-      <div className="lawyer-card-content">
-        <div className="lawyer-card-profile">
-          {/* Add your lawyer's profile image here */}
-        </div>
-        <div className="lawyer-card-details">
-          <h2>{lawyerData.name}</h2>
-          <p>{lawyerData.address}</p>
-          <p>Experience: {lawyerData.experience}</p>
-          <p className="languages-section">Languages: {lawyerData.languages.join(", ")}</p> {/* Display languages */}
-          <div className="lawyer-card-skills">
-            {lawyerData.skills.map((skill, index) => (
-              <span key={index} className="skill-tag">
-                {skill}
-              </span>
-            ))}
-          </div>
-          <div className="lawyer-card-options">
-            <button className="call-button">Call</button>
-            <button className="message-button">Message</button>
-          </div>
-        </div>
+      <div className="card-area">
+      {/* {lawyerDataArray.map((lawyerData, index) => (
+      ))}  */}
+      {/* <Lcard lawyerData={lawyerData} /> */}
       </div>
+      <div>{selectedOptions}</div>
     </div>
   );
 };
---
+++ SIH_PROJECT/src/Components/Lcard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "../Components/style/LawyearCard.css"; 
+
+const Lcard = ({lawyerData}:any) => {
+  return (
+    <div className="lawyer-card">
+      <div className="lawyer-card-header">
+        <div className="lawyer-card-rating">
+          Rating: 4
+        </div>
+        <div className="lawyer-card-reviews">
+          Review: 36
+        </div>
+      </div>
+      <div className="lawyer-card-content">
+        <div className="lawyer-card-profile">
+          {/* Add your lawyer's profile image here */}
+        </div>
+        <div className="lawyer-card-details">
+          <h2>{lawyerData.FIRSTNAME}</h2>
+          <p>{lawyerData.LOCATION}</p>
+          <p>Experience: {lawyerData.EXPERIENCE}</p>
+          <p className="languages-section">Languages: {lawyerData.LANGUAGES.join(", ")}</p> {/* Display languages */}
+          <div className="lawyer-card-skills">
+            {lawyerData.SPECIALITIES.map((skill:any, index:any) => (
+              <span key={index} className="skill-tag">
+                {skill}
+              </span>
+            ))}
+          </div>
+          <div className="lawyer-card-options">
+            <button className="call-button">Call</button>
+            <button className="message-button">Message</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Lcard;
