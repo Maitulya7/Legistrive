6141f7f445d14c6663d1182ff4f97d57e352c930
Maitulya Vaghela <vaghelamaitulya85@gmail.com>
2023-09-20 15:27:32 +0530

    Update LawyerCard.tsx

--- SIH_PROJECT/src/Components/LawyerCard.tsx
+++ SIH_PROJECT/src/Components/LawyerCard.tsx
@@ -1,48 +1,99 @@
-import React from "react";
-import "../Components/style/LawyearCard.css"; 
+import React, { useState, } from "react";
+import "../Components/style/LawyearCard.css";
+import Lcard from "./Lcard";
+// import axios from "axios";
+import UserNavbar from "./UserNavbar";
 
-const LawyerCard = () => {
-  // Dummy lawyer data
-  const lawyerData = {
-    name: "John Doe",
-    address: "123 Main St, City",
-    experience: "5 years",
-    skills: ["Divorce", "Child Custody", "Mediation"],
-    rating: 4.5,
-    reviews: 27,
-    languages: ["English", "Spanish"], // Add a languages property
+interface LawyerData {
+  name: string;
+  address: string;
+  experience: string;
+  skills: string[];
+  rating: number;
+  reviews: number;
+  languages: string[];
+}
+
+const LawyerCard: React.FC = () => {
+  const [, ] = useState<string[]>([]);
+  // const [location, setLocation] = useState<string[]>([]);
+  // const [, setLanguage] = useState<string[]>([]);
+  const [lawyerDataArray, ] = useState<LawyerData[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [, setDoFilter] = useState<boolean>(false);
+
+  // ... Rest of your code ...
+
+  // const handleCategory = (option: string) => {
+  //   setSelectedOptions((prevSelectedOptions) => {
+  //     if (prevSelectedOptions.includes(option)) {
+  //       return prevSelectedOptions.filter((item) => item !== option);
+  //     } else {
+  //       return [...prevSelectedOptions, option];
+  //     }
+  //   });
+  //   setDoFilter(true);
+  // };
+
+  // const handleCity = (option: string) => {
+  //   setLocation((prevSelectedCities) => {
+  //     if (prevSelectedCities.includes(option)) {
+  //       return prevSelectedCities.filter((item) => item !== option);
+  //     } else {
+  //       return [...prevSelectedCities, option];
+  //     }
+  //   });
+  //   setDoFilter(true);
+  // };
+
+  // const handleLanguage = (option: string) => {
+  //   setLanguage((prevSelectedLanguages) => {
+  //     if (prevSelectedLanguages.includes(option)) {
+  //       return prevSelectedLanguages.filter((item) => item !== option);
+  //     } else {
+  //       return [...prevSelectedLanguages, option];
+  //     }
+  //   });
+  //   setDoFilter(true);
+  // };
+
+  const onSearch = (query: string) => {
+    setSearchTerm(query);
+    setDoFilter(true);
   };
 
-  return (
-    <div className="lawyer-card">
-      <div className="lawyer-card-header">
-        <div className="lawyer-card-rating">
-          Rating: {lawyerData.rating}
-        </div>
-        <div className="lawyer-card-reviews">
-          Review: {lawyerData.reviews}
-        </div>
-      </div>
-      <div className="card-area">
-        <div className="search-bar">
-          <input
-            type="text"
-            placeholder="Search..."
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <button onClick={() => onSearch(searchTerm)}>Search</button>
-        </div>
+  // ... Rest of your code ...
 
-        {lawyerDataArray.map((lawyerData, index) => (
-           <Lcard lawyerData={lawyerData} /> 
-      ))} 
-        
+  return (
+    <>
+      <UserNavbar />
+      <div className="lawyer-data">
+        <div className="category-bar">
+          {/* <select value={location} onChange={(e) => handleCity(e.target.value)}>
+            <option value="">Select a city</option>
+            {cities.map((city) => (
+              <option key={city} value={city}>
+                {city}
+              </option>
+            ))}
+          </select> */}
+          {/* ... */}
+        </div>
+        <div className="card-area">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={(e) => onSearch(e.target.value)}
+            />
+          </div>
+          {lawyerDataArray.map((lawyerData, index) => (
+            <Lcard key={index} lawyerData={lawyerData} />
+          ))}
+        </div>
       </div>
-      <div>{selectedOptions}</div>
-    </div>
     </>
-   
   );
 };
 
